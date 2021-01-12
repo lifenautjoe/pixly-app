b0ee20c70cbd82b4bcf22ae3e25caaf6870fb4d9
Joel Hernandez <joel@open-book.org>
2021-01-12 22:42:03 +0100

    :sparkles: add AuthSplash

--- src/components/AuthSplash/AuthSplash.tsx
+++ src/components/AuthSplash/AuthSplash.tsx
@@ -1,69 +1,69 @@
 import { FormEvent, FunctionComponent, useState } from "react";
 import React from "react";
-import "./AuthModal.scss";
+import "./AuthSplash.scss";
 import { useStore } from "../../hooks/stores";
 import { AvatarPicker } from "../AvatarPicker/AvatarPicker";
-//import { useStore } from "../../hooks/stores";
 
-interface AuthModalProps {
+interface AuthSplashProps {
   roomName: string;
 }
 
-export const AuthModal: FunctionComponent<AuthModalProps> = ({ roomName }: AuthModalProps) => {
+export const AuthSplash: FunctionComponent<AuthSplashProps> = ({ roomName }: AuthSplashProps) => {
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const appStore = useStore("appStore");
+  const appStore = useStore("pixlyStore");
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
+    setErrorMessage("");
 
-    const validationErrors = appStore.validateUserAuthenticationData(name, avatar);
+    const authenticateDto = {
+      name,
+      avatar,
+    };
+
+    const validationErrors = appStore.validateUserAuthenticationDto(authenticateDto);
+
     if (validationErrors.length > 0) {
-      console.log(validationErrors);
-    } else {
-      console.log("Good name");
-    }
-  };
+      const firstValidationErrorConstraints = validationErrors[0].constraints;
 
-  const onPickedAvatar = (avatar: string) => {
-    setAvatar(avatar);
+      const firstErrorMessage = Object.values(firstValidationErrorConstraints as Record<string, string>)[0];
+
+      setErrorMessage(firstErrorMessage);
+    } else {
+      appStore.authenticate(authenticateDto);
+    }
   };
 
   return (
     <div
-      className={"AuthModal"}
+      className={"AuthSplash"}
       style={
         {
           //backgroundImage: `url(${Background})`,
         }
       }
     >
-      <form className="AuthModalForm" onSubmit={handleSubmit}>
+      <form className="AuthSplashForm" onSubmit={handleSubmit}>
         <div className="nes-container with-title is-centered">
           <p className="title">Welcome to Pixly!</p>
           <p>{`So you'd like to join room ${roomName} huh?`}</p>
           <div className="nes-field">
             <label htmlFor="name_field">{"What's your name?"}</label>
-            <input
-              type="text"
-              id="name_field"
-              className={`nes-input ${errorMessage ? "is-error" : ""}`}
-              value={name}
-              onChange={e => setName(e.target.value)}
-            ></input>
+            <input type="text" id="name_field" className="nes-input" value={name} onChange={e => setName(e.target.value)}></input>
           </div>
-          <div className="AuthModalFormAvatars">
+          <div className="AuthSplashFormAvatars">
             <span className="nes-text">{"Pick an avatar"}</span>
-            <AvatarPicker onPickedAvatar={onPickedAvatar} pickedAvatar={avatar} />
+            <AvatarPicker onPickedAvatar={avatar => setAvatar(avatar)} pickedAvatar={avatar} />
           </div>
           {errorMessage && (
-            <div className="AuthModalFormErrors">
-              <span className="nes-text is-error">{errorMessage}</span>
+            <div className="AuthSplashFormErrors">
+              <p className="nes-text is-error">{errorMessage}</p>
             </div>
           )}
 
-          <div className="AuthModalFormButton">
+          <div className="AuthSplashFormButton">
             <button type="submit" className="nes-btn is-primary">
               Join room
             </button>
